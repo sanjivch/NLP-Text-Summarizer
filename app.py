e0c2455a15e14cc5cee4b518317ab67e10c88c7f
sanjivch <sanjiv.ch09@gmail.com>
2022-07-31 15:44:45 +0530

    Changed the port

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello World Sanjiv!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=33507)
