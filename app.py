6a8f31957914d09e1468722f1837a2be8f4a531f
sanjivch <sanjiv.ch09@gmail.com>
2022-08-04 23:15:57 +0530

    Modified app logic

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import gunicorn
-#from requests import request
 import spacy 
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+from collections import Counter
+from heapq import nlargest
+from heapq import nlargest
 
 try:
     nlp = spacy.load("en_core_web_sm")
@@ -9,47 +13,60 @@ except: # If not present, we download
     spacy.cli.download("en_core_web_sm")
     nlp = spacy.load("en_core_web_sm")
 
-# Pkgs for Normalizing Text
-from spacy.lang.en.stop_words import STOP_WORDS
-from heapq import nlargest
-
-def text_summarizer(raw_docx):
-    raw_text = raw_docx
+def text_summarizer(raw_text):
+    '''
+    Summarizes a text
+    '''
+    
     docx = nlp(raw_text)
-    stopwords = list(STOP_WORDS)
-    # Build Word Frequency # word.text is tokenization in spacy
-    word_frequencies = {}  
-    for word in docx:  
-        if word.text not in stopwords:
-            if word.text not in word_frequencies.keys():
-                word_frequencies[word.text] = 1
-            else:
-                word_frequencies[word.text] += 1
+    keyword = list()
+    words = list()
+    stop_words = list(STOP_WORDS)
+    
+    pos_tag = ['PROPN', 'ADJ', 'NOUN', 'VERB']
+
+    for token in docx:
+        #print(token)
+        if (token.text not in punctuation):
+            words.append(token.text)
+        if (token.text in stop_words) or (token.text in punctuation):
+            continue
+        if token.pos_ in pos_tag:
+            keyword.append(token.text)
+    # Build Word Frequency 
+    # word.text is tokenization in spacy    
+    freq_word = Counter(keyword) 
+
+    # Normalize
+    max_freq = freq_word.most_common(1)[0][1]
+
+    for word in freq_word.keys():
+        freq_word[word] = freq_word[word]/max_freq
 
 
-    maximum_frequncy = max(word_frequencies.values())
-
-    for word in word_frequencies.keys():  
-        word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
-    # Sentence Tokens
-    sentence_list = [ sentence for sentence in docx.sents ]
+ 
+    sentence_scores = dict()
+    
+   
 
     # Sentence Scores
-    sentence_scores = {}  
-    for sent in sentence_list:  
-        for word in sent:
-            if word.text.lower() in word_frequencies.keys():
-                if len(sent.text.split(' ')) < 30:
-                    if sent not in sentence_scores.keys():
-                        sentence_scores[sent] = word_frequencies[word.text.lower()]
-                    else:
-                        sentence_scores[sent] += word_frequencies[word.text.lower()]
+    for sentence in docx.sents:
+      
+        for word in sentence:
+            #print(word)
+            if word.text in freq_word.keys():
+                if sentence in sentence_scores.keys():
+                    sentence_scores[sentence] += freq_word[word.text]
+                else:
+                    sentence_scores[sentence] = freq_word[word.text]
 
 
-    summarized_sentences = nlargest(7, sentence_scores, key=sentence_scores.get)
-    final_sentences = [ w.text for w in summarized_sentences ]
-    summary = ' '.join(final_sentences)
-    return summary
+
+    summarized_sentences = nlargest(3, sentence_scores, key=sentence_scores.get)
+    
+    summary = [ w.text for w in summarized_sentences ]
+    final_summary = ' '.join(summary)
+    return len(words), final_summary
 
 
 
@@ -63,22 +80,9 @@ def index():
 def summarize_text():
     if request.method == 'POST':
         input_text = request.form['input-text']
-        summarized_text = text_summarizer(input_text)
+        word_count, summarized_text = text_summarizer(input_text)
 
-    return render_template('index.html', summarized_text=summarized_text)
-
-
-# @app.route('/analyze',methods=['GET','POST'])
-# def analyze():
-# 	start = time.time()
-# 	if request.method == 'POST':
-# 		rawtext = request.form['rawtext']
-# 		final_reading_time = readingTime(rawtext)
-# 		final_summary = text_summarizer(rawtext)
-# 		summary_reading_time = readingTime(final_summary)
-# 		end = time.time()
-# 		final_time = end-start
-# 	return render_template('index.html',ctext=rawtext,final_summary=final_summary,final_time=final_time,final_reading_time=final_reading_time,summary_reading_time=summary_reading_time)
+    return render_template('index.html', input_text=input_text, summarized_text=summarized_text, word_count = word_count)
 
 
 if __name__ == '__main__':
